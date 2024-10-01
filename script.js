7a6b65e376da1fab9ea172c3a4d1cb8bd39fa2cb
afanlyba <lubovafan@icloud.com>
2024-10-01 17:33:11 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const hello = function (name) {
+    console.log("Я учу " + name + "!");
+};
+ hello("JavaScript");
+
+
+
+function addImage() {
+    const cat1 = document.getElementById ('cat1') ;
+    cat1.src='cat2.jpg';
+}
+ 
+function changeImage() {
+    const cat1 = document.getElementById ('cat1') ;
+    cat1.src='cat1.jpg';
+}
